f26c0be86e7f534996f62646f9015da504515d11
Pär Björklund <per.bjorklund@gmail.com>
2021-06-15 07:59:05 +0200

    Fix syntax error
    Somehow this worked on my machine but not on github runner

--- cmake/ConnectorName.cmake
+++ cmake/ConnectorName.cmake
@@ -5,24 +5,36 @@
 #  BSD license.
 #  For details see the COPYING-CMAKE-SCRIPTS file.
 #
+
+include(CheckSymbolExists)
+
+check_symbol_exists(_X86_ "Windows.h" _X86_)
+check_symbol_exists(_AMD64_ "Windows.h" _AMD64_)
+check_symbol_exists(_ARM_ "Windows.h" _ARM_)
+check_symbol_exists(_ARM64_ "Windows.h" _ARM64_)
+
 MACRO(GET_CONNECTOR_PACKAGE_NAME name)
-# check if we have 64bit
-IF(SIZEOF_VOIDP EQUAL 8)
-  SET(IS64 1)
-ENDIF()
 
 SET (PLAFORM_NAME CMAKE_SYSTEM_NAME)
 SET (MACHINE_NAME CMAKE_SYSTEM_PROCESSOR)
 SET (CONCAT_SIGN "-")
 
 IF(CMAKE_SYSTEM_NAME MATCHES "Windows")
-  SET(PLATFORM_NAME "win")
-  SET(CONCAT_SIGN "")
-  IF(IS64)
-    SET(MACHINE_NAME "x64")
+  IF(CMAKE_SYSTEM_NAME MATCHES "WindowsStore")
+    SET(PLATFORM_NAME "winstore")
   ELSE()
+    SET(PLATFORM_NAME "win")
+  ENDIF()
+  SET(CONCAT_SIGN "")
+  IF(_x86_)
     SET(MACHINE_NAME "32")
-  END()
+  ELSEIF(_AMD64_)
+    SET(MACHINE_NAME "x64")
+  ELSEIF(_ARM_)
+    SET(MACHINE_NAME "arm")
+  ELSEIF(_ARM64_)
+    SET(MACHINE_NAME "arm64")
+  ENDIF()
 ENDIF()
 
 SET(product_name "mysql-connector-c-${CPACK_PACKAGE_VERSION}-${PLATFORM_NAME}${CONCAT_SIGN}${MACHINE_NAME}")
